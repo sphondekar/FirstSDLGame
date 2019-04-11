2d388958ab3ee97f1996220676bc4ecd54605e3b
Sahil-Phondekar <sahilphondekar@gmail.com>
2019-04-12 00:44:41 +0530

    step3-Adding image to the screen

--- dotgame.c
+++ dotgame.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<SDL.h>
+#include <SDL_image.h>
 
 int main(int argc, char* argv[])
 {
@@ -32,11 +33,93 @@ int main(int argc, char* argv[])
 	    return 1;
     }
     
+    
+    // create a renderer, which sets up the graphics hardware using SDL_CreateRenderer
+    //SYNTAX : SDL_CreateRenderer( SDL_Window* window, int index, Uint32 flags)
+    //window = pass the created window
+    //index = index of the rendering driver or set it to -1 
+    //flags = 0, or can use one or more flags using bitwise or operator
+    //		SDL_RENDERER_SOFTWARE - the renderer is a software fallback
+	//		SDL_RENDERER_ACCELERATED - the renderer uses hardware acceleratioon
+	//		SDL_RENDERER_PRESENTVSYNC - present is synchronised with the refresh rate
+	// 		SDL_RENDERER_TARGETTEXTURE - the renderer supports rendering to texture
+    Uint32 render_flags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
+    SDL_Renderer* rend = SDL_CreateRenderer(win, -1, render_flags);
+    
+    //check if renderer is created or not
+    if (!rend)
+    {
+      printf("error creating renderer: %s\n", SDL_GetError());
+      SDL_DestroyWindow(win);
+      SDL_Quit();
+      return 1;
+    }
+    
+    // load the image into memory using SDL_image library function
+    // A window consists of a surface (and be an image)
+    // We load the image by using IMG_Load("path to image")
+    SDL_Surface* surface = IMG_Load("resources/background.png");
+    
+    //check if image is loaded or not
+    if (!surface)
+    {
+        printf("error creating surface\n");
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    
+    // load the image data into the graphics hardware's memory
+    // Once the surface is created we create a texture from that surface
+    // The reason for creating texture from the surface is :
+	// 1.SDL_Surface is just a struct containing pixel information, while SDL_Texture
+	//		is an efficient, driver specific representation of pixel data.
+	// 2.SDL_Surface uses software rendering (via CPU), while SDL_Texture uses hardware rendering
+    //We use SDL_CreateTextureFromSurface(renderer , surface)
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(rend, surface);
+    
+    //check if texture is successfully created or not
+    if (!tex)
+    {
+        printf("error creating texture: %s\n", SDL_GetError());
+        SDL_DestroyRenderer(rend);
+        SDL_DestroyWindow(win);
+        SDL_Quit();
+        return 1;
+    }
+    
+    //Once texture is created from the surface we can release the surface 
+    SDL_FreeSurface(surface);
+    
+	// clear the window
+	// during starting, the renderer is filled with junk pixel data
+	// uptill now the everything (texture/surface) was created behind the scenes
+	// SDL_RenderClear will clear the junk data and show the created texture.
+    SDL_RenderClear(rend);
+    
+    
+    // draw the image to the window
+    // SDL_RenderCopy - copy a portion of the texture to the current rendering target.
+    // SDL_RenderCopy(renderer, source texture , scrrect , dstrect)
+    // srcrect - the source SDL_Rect structure or NULL
+    // dstrect - the destination SDL_Rect structure or NULL.
+    SDL_RenderCopy(rend, tex, NULL, NULL);
+    
+    // use this function to update the screen with any rendering performed
+    // Double buffer:  1. backbuffer   2. frontbuffer
+    // when we draw something it is drawn on the back buffer and is not visible
+	// using SDL_RenderPresent(renderer) we send the image on the frontbuffer
+	// this makes the changes visible in the window 
+    SDL_RenderPresent(rend);
+    
     // wait a few seconds
     SDL_Delay(5000);
     
     //Call this function to shutdown each initialized subsystem
     //clean all resources before shutdown
+    SDL_DestroyTexture(tex);
+    SDL_DestroyRenderer(rend);
     SDL_DestroyWindow(win);
     SDL_Quit();
 }
